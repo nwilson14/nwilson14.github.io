1bdc90337f5e76a3458e2dc535ac251b087952a2
mchun2016 <mchun2016@my.fit.edu>
2022-05-28 17:31:06 +0200

    bleh

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={heart} className="heart-logo" alt="logo" />
         <p>
-        ❤️ Hey cutie ❤️
+        ❤️ Hey CUTIE ❤️
         </p>
         <a
           className="hugs-link"
